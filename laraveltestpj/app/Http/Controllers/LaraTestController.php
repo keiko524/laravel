57870af85ec4462a53ce564d7b044e61af1bbb70
f.keiko <clownowl.hoot.hoot.coo524@gmail.com>
2021-09-05 15:38:52 +0900

    first

---
+++ laraveltestpj/app/Http/Controllers/LaraTestController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LaraTestController extends Controller
+{
+  public function index() {
+        return '建物です';
+    }
+
+
+  public function room($room) {
+        return '部屋の番号は' . $room . 'です';
+  }
+
+}
+
+
